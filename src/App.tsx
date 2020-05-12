6de2597df3e23ba4aa847094991881776c5d5086
David Dembo <dembonanza@gmail.com>
2020-05-12 13:10:07 +1000

    clean up repo, tweak lint config, implement core app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-
-import logo from './logo.svg';
-import './App.scss';
+import styles from './App.module.css';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
+  <div className={styles.wrapper}>
+    <h1>App content placeholder</h1>
   </div>
 );
 
