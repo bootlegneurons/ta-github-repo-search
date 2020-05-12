9bb3c9bbe5c312ff32cd1bc4a1a41fb7492de18d
David Dembo <dembonanza@gmail.com>
2020-05-12 13:16:15 +1000

    Add routing for /repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import styles from './App.module.css';
+
+const REPOS_PATH = '/repositories';
 
 const App: React.FC = () => (
   <div className={styles.wrapper}>
-    <h1>App content placeholder</h1>
+    <main className={styles.page}>
+      <Router>
+        <Switch>
+          <Route path={REPOS_PATH}>
+            <h1>Repositories placeholder</h1>
+          </Route>
+          <Redirect to={REPOS_PATH} />
+        </Switch>
+      </Router>
+    </main>
   </div>
 );
 
