b6c8faf605141955be89a99dd290260d575daf7d
David Dembo <dembonanza@gmail.com>
2020-05-12 13:38:13 +1000

    handle search input, update route with search term on submit

--- src/components/views/Repositories/Repositories.tsx
+++ src/components/views/Repositories/Repositories.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchInput from './SearchInput';
 import SearchResults from './SearchResults';
 
-const Repositories: React.FC = () => (
-  <>
-    <h1>Repository Search</h1>
-    <SearchInput />
-    <h2>Results</h2>
-    <SearchResults />
-  </>
-);
+const Repositories: React.FC = () => {
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  return (
+    <>
+      <h1>Repository Search</h1>
+      <SearchInput
+        onChange={(value): void => setSearchInput(value)}
+        resultsPath="/repositories"
+        value={searchInput}
+      />
+      <h2>Results</h2>
+      <SearchResults />
+    </>
+  );
+};
 
 export default Repositories;
--- src/components/views/Repositories/SearchInput.tsx
+++ src/components/views/Repositories/SearchInput.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
 
-const SearchInput: React.FC = () => <p>Input placeholder</p>;
+type SearchInputProps = {
+  onChange?: (v: string) => void;
+  resultsPath: string;
+  value: string;
+};
+
+const SearchInput: React.FC<SearchInputProps> = ({ onChange, resultsPath, value }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (onChange) {
+      onChange(e.target.value);
+    }
+  };
+
+  return (
+    <form>
+      <label htmlFor="searchInput">
+        <p>Search:</p>
+        <input id="searchInput" onChange={handleChange} type="text" value={value} />
+      </label>
+      <Link to={`${resultsPath}?q=${value}`}>
+        <button type="submit">Search</button>
+      </Link>
+    </form>
+  );
+};
 
 export default SearchInput;
